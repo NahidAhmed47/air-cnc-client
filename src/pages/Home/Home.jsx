337615f29d56f401ce944c3c1066715116e5dc8e
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-27 19:29:55 +0600

    rooms card and section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Categories from '../../components/categories/Categories'
-// import Rooms from '../../components/Rooms/Rooms'
+import Rooms from '../../components/rooms/rooms'
 
 const Home = () => {
   return (
     <>
       <Categories />
-      {/* <Rooms /> */}
+      <Rooms />
     </>
   )
 }
